ea805b03b651ec759add7d3052465a22a7c53a1f
FollowTheDopamine <followthedopamine@gmail.com>
2025-11-04 20:54:38 +1300

    Fixed a bug where cards could be dragged from the hand into packs. Fixed a bug where cards couldn't be moved into an empty hand General tidy up of the implementation of dragging cards from packs to hand

--- scenes/cards/Card.gd
+++ scenes/cards/Card.gd
@@ -25,6 +25,7 @@ var activated := false
 var held_card: Card
 var hovered_card: Card
 
+const DISABLED_COLOUR: Color = Color(0.08, 0.08, 0.08, 1.0)
 
 const colours := [
 	Color(255, 0, 0),
@@ -39,9 +40,7 @@ var colour: Color;
 func _ready() -> void:
 	set_process_input(false)
 
-	var new_style: StyleBoxFlat = panel.get_theme_stylebox("panel").duplicate()
-	new_style.bg_color = colour
-	panel.add_theme_stylebox_override("panel", new_style)
+	change_colour(colour)
 
 	timer_label.text = "%.1f" % duration
 	
@@ -55,29 +54,27 @@ func _process(delta: float) -> void:
 		time_remaining -= delta
 		timer_label.text = "%.1f" % time_remaining
 		timer_spinner.rotation += delta * 10
-		
-	# This is looping on every single card which probably isn't the most
-	# efficient way to do things. 
 	
-	# I tried to put similar code in _get_drag_data but it never updates the
-	# hovered card inside that callback.
-	var mousePos = get_viewport().get_mouse_position()
-	if !self.get_global_rect().has_point(mousePos):
-		hide_switch_interface()
-		return
+	# This looping on every single card which probably isn't the most
+	# efficient way to do things. 
+	handle_switch_interface()
 		
-	if held_card == null:
-		return
-		
-	if held_card.get_parent() != self.get_parent():
-		show_switch_interface()
+func _drop_data(_at_position: Vector2, data: Variant) -> void:
+	if !data.get_parent() is Hand:
+		switch_cards(data, self)
 	else:
-		hide_switch_interface()
+		SignalBus.card_chosen.emit(self)
 
 func _can_drop_data(_at_position: Vector2, _data: Variant) -> bool:
-	return true
+	# Trying to swap with a half activated card seems like a bad idea
+	if !self.get_parent() is Hand:
+		return false
+	return !activated
 
 func _get_drag_data(at_position: Vector2) -> Variant:
+	# Prevent currently activated cards from being dragged
+	if activated:
+		return null
 	var preview_parent = Control.new()
 
 	dragging_node = self.duplicate()
@@ -99,29 +96,24 @@ func _get_drag_data(at_position: Vector2) -> Variant:
 	dragging = true
 	SignalBus.card_dragged.emit(self)
 	
-	# This doesn't print when a new card is hovered
-	#if hovered_card.get_parent() != self.get_parent():
-		#print(hovered_card)
-		#hovered_card.show_switch_interface()
 	return self
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion or event is InputEventScreenDrag:
 		if PlayerManager.hand_size != PlayerManager.max_hand_size:
-			# This should ideally not reference hovered card parent and instead should be the hand
-			if self.get_parent() != hovered_card.get_parent():
-				self.reparent(hovered_card.get_parent())
-		# if dragging left/right update the position in the hbox (hand)
+				# This should ideally not reference hovered card parent and instead should be the hand
+				if !self.get_parent() is Hand:
+					self.reparent(hovered_card.get_parent())
+		# if dragging left/right update the position in the parent
 		if dragging_node.global_position.x < global_position.x - threshold:
 			if self.get_index() > 0:
 				# Have to use get_parent() here instead of hbox because the parent
-				# of the card can change.
+				# of the card can change while dragging.
 				get_parent().move_child(self, self.get_index() - 1)
 		elif dragging_node.global_position.x > global_position.x + threshold:
-			if self.get_index() < get_parent().get_child_count() - 2:
+			if self.get_index() < get_parent().get_child_count() - 1:
 				get_parent().move_child(self, self.get_index() + 1)
 				
-	
 
 	# if dragging left/right, give it a lil rotation
 	if last_position.x > dragging_node.global_position.x:
@@ -131,16 +123,7 @@ func _input(event: InputEvent) -> void:
 	last_position = dragging_node.global_position
 	
 	
-	
-
 	if dragging and event.is_action_released("click"):
-		hide_switch_interface()
-		if hovered_card.get_parent() != self.get_parent():
-			# Trying to swap with a half activated card seems like a bad idea
-			if !hovered_card.activated:
-				switch_cards(self, hovered_card)
-		else:
-			SignalBus.card_chosen.emit(self)
 		held_card = null
 		dragging = false
 		panel.show()
@@ -158,18 +141,35 @@ func _on_mouse_entered() -> void:
 func _on_card_hovered(card: Card) -> void:
 	if card != held_card:
 		hovered_card = card
+
+func handle_switch_interface() -> void:
+	if dragging:
+		return
+	if activated:
+		hide_switch_interface()
+		return
+	if !get_parent() is Hand:
+		hide_switch_interface()
+		return
 		
-#func add_card_to_hand(new_card: Card, old_card: Card) -> void:
-	
+	var mousePos = get_viewport().get_mouse_position()
+	if !self.get_global_rect().has_point(mousePos):
+		hide_switch_interface()
+		return
 		
+	if held_card == null:
+		return
+		
+	if held_card.get_parent() != self.get_parent():
+		show_switch_interface()
+	else:
+		hide_switch_interface()
+
 func switch_cards(new_card: Card, old_card: Card) -> void:
 	new_card.reparent(old_card.get_parent())
 	old_card.get_parent().move_child(new_card, old_card.get_index())
-	# Used to remove card from hand or Card.completed signal will crash
-	SignalBus.card_discarded.emit(old_card)
 	SignalBus.card_chosen.emit(new_card)
-	# Cleanup of the card happens in Hand.gd so we can remove it from the card
-	# array.
+	old_card.queue_free()
 	
 func show_switch_interface() -> void:
 	switch_interface.visible = true
@@ -179,11 +179,18 @@ func hide_switch_interface() -> void:
 
 func set_colour(index: int):
 	colour = colours[index % colours.size()]
+	
+func change_colour(new_colour: Color) -> void:
+	var new_style: StyleBoxFlat = panel.get_theme_stylebox("panel").duplicate()
+	new_style.bg_color = new_colour
+	panel.add_theme_stylebox_override("panel", new_style)
 
 func activate():
 	timer.start(duration)
 
 	panel.position.y = -10
+	change_colour(DISABLED_COLOUR)
+	
 	time_remaining = duration
 
 	timer_label.show()
@@ -195,6 +202,7 @@ func deactivate():
 	timer.stop()
 
 	panel.position.y = 0
+	change_colour(colour)
 
 	timer_label.hide()
 	timer_spinner.hide()
